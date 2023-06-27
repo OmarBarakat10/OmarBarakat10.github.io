2092366bd40fb9d3a75d0a9de0f0d0971a361d19
OmarBarakat10 <ob188101@gmail.com>
2023-06-27 16:06:00 +0000

    saving sorting exercise

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -14,15 +14,69 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array) {
+    for(i = 0; i <= array.length - 1; i++){
+        for(j = array.length - 1; j > i + 1; j--){
+            if (array[j].value < array[j-1].value){
+                swap(array, j - 1, j)
+                updateCounter(bubbleCounter);
+                await sleep();
+            }
+        }
+    }
+}//sorts the array by comparing 2 values. it swaps the values based on which is greater and which is less
 
 // TODO 3: Implement quickSort
+
+async function quickSort(array, left, right){
+    if((right - left) > 0){
+        var index = await partition(array, left, right)
+    
+        if(left < (index - 1)){
+        await quickSort(array, left, index - 1)
+        }
+        if(index < right){
+        await quickSort(array, index, right)
+        }
+    }
+}//this is the function that quick sorts by determining what indexes are greater and what indexes are less
 
 
 // TODOs 4 & 5: Implement partition
 
+async function partition(array, left, right){
+    pivot = array[Math.floor((right + left)/2)].value;
+        while(left < right){
+            while(array[left].value < pivot) { 
+                left++ 
+            }
+            while(array[right].value > pivot) { 
+                right-- 
+            }
+            if(left < right){
+                swap(array, left, right);
+                updateCounter(quickCounter);
+                await sleep();
+            }
+
+        }
+  return(left + 1);
+}// this is the function that actually sorts the array by checking which values are greater nad moving left and right
+
 
 // TODO 1: Implement swap
+
+function swap(array, i, j) {
+    var temp1 = array[i]
+    var temp2 = array [j] //assigns i and j to temporary variables
+
+    array[i] = temp2
+    array[j] = temp1 // switches i and j by assigning to opposite variables
+
+
+    drawSwap(array, i, j);
+}; //the swap function that swaps 2 indexes of an array
+
 
 
 ///////////////////////////////////////////////////////////////////////
